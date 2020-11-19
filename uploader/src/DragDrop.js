49d0cfd7d6f33dcbd279e068898daba2d3ee29da
jcybertech <covingtonjoshua7@gmail.com>
2020-11-18 21:22:44 -0700

    working on implementing react hooks without calling them in a separate class.

--- uploader/src/DragDrop.js
+++ uploader/src/DragDrop.js
@@ -4,15 +4,68 @@ import "./DragDrop.css";
 import Background from "./Background";
 
 const DragDrop = () => {
+  const dragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const dragEnter = (e) => {
+    e.preventDefault();
+  };
+
+  const dragLeave = (e) => {
+    e.preventDefault();
+  };
+
+  const fileDrop = (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    if (files.length) {
+      handleFiles(files);
+    }
+  };
+
+  const handleFiles = (files) => {
+    for (let i = 0; i < files.length; i++) {
+      if (validateFile(files[i])) {
+        // add to an array to display the name of the file
+      } else {
+        // add a new property called invalid
+        // add to the same array to display name of the file
+        //set error message
+      }
+    }
+  };
+
+  const validateFile = (file) => {
+    const validTypes = [
+      "image/jpeg",
+      "image/jpg",
+      "image/png",
+      "image/gif",
+      "image/x-icon",
+    ];
+    if (validTypes.indexOf(file.type) === -1) {
+      return false;
+    }
+    return true;
+  };
   return (
-    <div className="main__container">
+    <div
+      className="main__container"
+      onDragOver={dragOver}
+      onDragEnter={dragEnter}
+      onDragLeave={dragLeave}
+      onDrop={fileDrop}
+    >
       <div className="drop__header">Upload your image</div>
       <div className="drop__second-header">File should be Jpeg, Png...</div>
       <div className="drop__container">
-        <Background />
-        <div className="drop__bottom">Drag & Drop your image here</div>
+        <div className="drop__image">
+          <Background />
+        </div>
+        <div className="drop__bottom-text">Drag & Drop your image here</div>
       </div>
-      <div className="drop__second-bottom">Or</div>
+      <div className="drop__bottom">Or</div>
       <div className="drop__footer">choose a file</div>
     </div>
   );
