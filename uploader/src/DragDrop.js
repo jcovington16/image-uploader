d54f6b6e2570b7fffd7c2f80e638912609ad17e1
jcybertech <covingtonjoshua7@gmail.com>
2020-12-11 00:02:26 -0700

    Started a new branch and adding a way to drop files and log it to the screen just to verify the file is there.

--- uploader/src/DragDrop.js
+++ uploader/src/DragDrop.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./DragDrop.css";
-import React, { useState } from "react";
 
 import Background from "./Background";
 
-const DragDrop = () => {
+// const DragDrop = () => {
+export default function DragDrop() {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [validFiles, setValidFiles] = useState([]);
+  const [unsupportedFiles, setUnsupportedFiles] = useState([]);
+
+  useEffect(() => {
+    let filteredArray = selectedFiles.reduce((file, current) => {
+      const x = file.find((item) => item.name === current.name);
+      if (!x) {
+        return file.concat([current]);
+      } else {
+        return file;
+      }
+    }, []);
+    setValidFiles([...filteredArray]);
+  }, [selectedFiles]);
+
   const dragOver = (e) => {
     e.preventDefault();
   };
@@ -20,6 +37,7 @@ const DragDrop = () => {
   const fileDrop = (e) => {
     e.preventDefault();
     const files = e.dataTransfer.files;
+    console.log(files);
     if (files.length) {
       handleFiles(files);
     }
@@ -29,10 +47,16 @@ const DragDrop = () => {
     for (let i = 0; i < files.length; i++) {
       if (validateFile(files[i])) {
         // add to an array to display the name of the file
+        setSelectedFiles((prevArray) => [...prevArray, files[i]]);
       } else {
         // add a new property called invalid
+        files[i]["invalid"] = true;
         // add to the same array to display name of the file
+        setSelectedFiles((prevArray) => [...prevArray, files[i]]);
         //set error message
+        setErrorMessage("File type not permitted");
+
+        setUnsupportedFiles((prevArray) => [...prevArray, files[i]]);
       }
     }
   };
@@ -50,6 +74,46 @@ const DragDrop = () => {
     }
     return true;
   };
+
+  const fileSize = (size) => {
+    if (size === 0) return "0 Bytes";
+    const k = 1024;
+    const sizes = ["bytes", "kb", "mb", "gb", "tb"];
+    const i = Math.floor(Math.log(size) / Math.log(k));
+    return parseFloat((size / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+  };
+
+  const fileType = (fileName) => {
+    return (
+      fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length) ||
+      fileName
+    );
+  };
+
+  const removeFile = (name) => {
+    // find the index of the item
+    // remove the item from array
+
+    const validFileIndex = validFiles.findIndex((e) => e.name === name);
+    validFiles.splice(validFileIndex, 1);
+    // update validFiles array
+    setValidFiles([...validFiles]);
+    const selectedFileIndex = selectedFiles.findIndex((e) => e.name === name);
+    selectedFiles.splice(selectedFileIndex, 1);
+    // update selectedFiles array
+    setSelectedFiles([...selectedFiles]);
+
+    // Each invalid file dropped by the user will be added to the array
+    const unsupportedFileIndex = unsupportedFiles.findIndex(
+      (e) => e.name === name
+    );
+    if (unsupportedFileIndex !== -1) {
+      unsupportedFiles.splice(unsupportedFileIndex, 1);
+      // update unsupportedFiles array
+      setUnsupportedFiles([...unsupportedFiles]);
+    }
+  };
+
   return (
     <div
       className="main__container"
@@ -70,6 +134,6 @@ const DragDrop = () => {
       <div className="drop__footer">choose a file</div>
     </div>
   );
-};
+}
 
-export default DragDrop;
+// export default DragDrop;
